543b8c8bfc29cc05a05462a4ff6d02f30bcf7064
Dan2a <dan.andrade313@gmail.com>
2022-02-16 15:58:56 -0300

    Teste

---
+++ 1-CalculoMedia/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteLogicoBLASTOFF
+{
+    class CaluloMedia
+    {
+        static void Main(String[] args)
+        {
+            double n1, n2, n3, n4, n5;
+            double media;
+
+            Console.WriteLine("Calulo de média");
+            Console.WriteLine("Digite os 5 valores para calcular a média:");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            n2 = Convert.ToDouble(Console.ReadLine());
+            n3 = Convert.ToDouble(Console.ReadLine());
+            n4 = Convert.ToDouble(Console.ReadLine());
+            n5 = Convert.ToDouble(Console.ReadLine());
+
+            media = (n1 + n2 + n3 + n4 + n5) / 5;
+
+            Console.WriteLine("A média é: " + media);
+        }
+    }
+}
